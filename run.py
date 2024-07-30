3c4f1d131efba20c8b2902b84e27a347ac984719
MR. AMIR <150195653+amirxd404@users.noreply.github.com>
2024-07-30 11:59:29 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 import os
+import subprocess
+import sys
+
+def install_and_import(package):
+    try:
+        __import__(package)
+    except ImportError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install_and_import('requests')
+
+import requests
 os.system('chmod 777 paid')
 os.system('./paid')
