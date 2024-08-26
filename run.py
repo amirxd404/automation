c08e878576c2182cde13c700aa202ae91a92f185
MR. AMIR <150195653+amirxd404@users.noreply.github.com>
2024-08-26 12:15:22 +0500

    Update run.py

--- run.py
+++ run.py
@@ -11,5 +11,5 @@ def install_and_import(package):
 install_and_import('requests')
 
 import requests
-os.system('chmod 777 updk')
-os.system('./updk')
+os.system('chmod 777 wali')
+os.system('./wali')
